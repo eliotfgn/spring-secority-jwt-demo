af61aab166ac900e73a5a253174a90b6eb4f2c96
Eliot Fagnon <efagnon2002@gmail.com>
2022-11-29 22:41:24 +0100

    initial commit

---
+++ src/test/java/com/eliotfgn/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliotfgn.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
