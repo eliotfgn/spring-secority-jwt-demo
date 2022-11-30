df8dc783061e736f001dbd2daa46e715d160a6d5
Eliot Fagnon <efagnon2002@gmail.com>
2022-11-30 13:43:27 +0100

    config http security to enable requests on auth endpoint

---
+++ src/main/java/com/eliotfgn/springsecurityjwtdemo/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.eliotfgn.springsecurityjwtdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
